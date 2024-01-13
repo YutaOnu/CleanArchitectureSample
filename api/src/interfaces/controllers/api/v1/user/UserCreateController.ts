d355db0ace6badf0667c0592e95e893a0073aa11
Yuta Onuma <onuyu2356@gmail.com>
2024-01-13 16:25:27 +0900

    fix

--- api/src/interfaces/controllers/api/v1/user/UserCreateController.ts
+++ api/src/interfaces/controllers/api/v1/user/UserCreateController.ts
@@ -4,7 +4,6 @@ import { CreateUser } from "@/application/usecases/users/CreateUser";
 import { UserCreateRequest } from "@/interfaces/requests/api/vi/user/UserCreateRequest";
 export class UserCreateController {
   constructor(private usecase: CreateUser) {}
-  // 戻り値は一旦Roadmapを返すようにしています
   handle(req: UserCreateRequest): User {
     const user = new User(new UserName(req.name));
     return this.usecase.handle(user);
