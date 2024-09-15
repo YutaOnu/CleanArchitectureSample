522a7075cd02c0955da63236ffdc8c7be23c5cf1
Yuta Onuma <onuyu2356@gmail.com>
2024-09-15 11:43:00 +0900

    fix: adapter修正

--- api/src/infra/http/express/adapters/request/IRequestAdapter.ts
+++
@@ -1,5 +0,0 @@
-import { Request as ExpressRequest } from "express";
-import { IRequest } from "@/interface/requests/api/v1/IRequest";
-export interface IRequestAdapter {
-  adapt(ExpressRequest: ExpressRequest): IRequest;
-}
--- api/src/infra/http/express/adapters/request/user/UserCreateRequestAdapter.ts
+++ api/src/infra/http/express/adapters/request/user/UserCreateRequestAdapter.ts
@@ -1,7 +1,7 @@
 import { Request as ExpressRequest } from "express";
-import { IRequestAdapter } from "@/infra/http/express/adapters/request/IRequestAdapter";
+import { IUserCreateRequestAdapter } from "@/interface/adapters/user/IUserCreateRequestAdapter";
 import { UserCreateRequest } from "@/interface/requests/api/v1/user/UserCreateRequest";
-export class UserCreateRequestAdapter implements IRequestAdapter {
+export class UserCreateRequestAdapter implements IUserCreateRequestAdapter {
   adapt(ExpressRequest: ExpressRequest) {
     const name = ExpressRequest.body.name;
     return new UserCreateRequest(name);
