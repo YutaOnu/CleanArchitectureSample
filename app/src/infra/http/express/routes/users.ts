7f649cc0d593bdc691f5c6c22c340d91a1cf60a6
Yuta Onuma <onuyu2356@gmail.com>
2024-09-21 17:28:12 +0900

    fix:  router修正

--- app/src/infra/http/express/routes/users.ts
+++ app/src/infra/http/express/routes/users.ts
@@ -4,25 +4,41 @@ import { UserInjector } from "@/infra/injectors/UserInjector";
 import { UserCreateRequestAdapter } from "@/infra/http/express/adapters/request/user/UserCreateRequestAdapter";
 import { InvalidArgumentError } from "@/errors/InvalidArgumentError";
 import { UserCreateValidator } from "@/infra/http/express/validators/user/UserCreateValidator";
-router.post("/", (req: express.Request, res: express.Response) => {
-  try {
-    UserCreateValidator.validate(req);
-    const adapter = new UserCreateRequestAdapter();
-    const userCreateRequest = adapter.adapt(req);
-    const controller = UserInjector.getUserCreateController();
-    const response = controller.handle(userCreateRequest);
-    // // ここは適当
-    res.json({
-      message: "User created successfully",
-      data: response, // ここに実際のレスポンスを含める
-    });
-  } catch (error: any) {
-    if (error instanceof InvalidArgumentError) {
-      res.status(400).send({ error: error.message });
-    } else {
-      res.status(500).send({ error: error.message });
+
+router.post(
+  "/",
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+      UserCreateValidator.validate(req);
+      const adapter = new UserCreateRequestAdapter();
+      const userCreateRequest = adapter.adapt(req);
+      const controller = UserInjector.getUserCreateController();
+      const response = controller.handle(userCreateRequest);
+
+      res.json({
+        message: "User created successfully",
+        data: response,
+      });
+    } catch (error: any) {
+      next(error);
     }
-    // TODO: その他エラー
   }
-});
+);
+
+// エラーハンドリングミドルウェア
+router.use(
+  (
+    error: any,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    if (error instanceof InvalidArgumentError) {
+      return res.status(400).json({ error: error.message });
+    }
+    // TODO: その他のエラー
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+);
+
 module.exports = router;
